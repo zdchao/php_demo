08b827cbc0cdad277fa9b806d0fb7010deaa2649
zhude <995949180.com>
2017-03-19 16:19:48 +0800

    demo3

---
+++ demo3/index.php
@@ -0,0 +1,81 @@
+<?php
+
+//filesize 解决大于2G 大小问题
+//http://stackoverflow.com/questions/5501451/php-x86-how-to-get-filesize-of-2-gb-file-without-external-program
+function get_filesize($path){
+	$result = false;
+	$fp = fopen($path,"r");
+	if(! $fp = fopen($path,"r")) return $return;
+	if(PHP_INT_SIZE >= 8 ){ //64bit
+		$result = (float)(abs(sprintf("%u",@filesize($path))));
+	}else{
+		if (fseek($fp, 0, SEEK_END) === 0) {
+			$result = 0.0;
+			$step = 0x7FFFFFFF;
+			while ($step > 0) {
+				if (fseek($fp, - $step, SEEK_CUR) === 0) {
+					$result += floatval($step);
+				} else {
+					$step >>= 1;
+				}
+			}
+		}else{
+			static $iswin;
+			if (!isset($iswin)) {
+				$iswin = (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN');
+			}
+			static $exec_works;
+			if (!isset($exec_works)) {
+				$exec_works = (function_exists('exec') && !ini_get('safe_mode') && @exec('echo EXEC') == 'EXEC');
+			}
+			if ($iswin && class_exists("COM")) {
+				try {
+					$fsobj = new COM('Scripting.FileSystemObject');
+					$f = $fsobj->GetFile( realpath($path) );
+					$size = $f->Size;
+				} catch (Exception $e) {
+					$size = null;
+				}
+				if (ctype_digit($size)) {
+					$result = $size;
+				}
+			}else if ($exec_works){
+				$cmd = ($iswin) ? "for %F in (\"$path\") do @echo %~zF" : "stat -c%s \"$path\"";
+				@exec($cmd, $output);
+				if (is_array($output) && ctype_digit($size = trim(implode("\n", $output)))) {
+					$result = $size;
+				}
+			}else{
+				$result = filesize($path);
+			}
+		}
+	}
+	fclose($fp);
+	return $result;
+}
+
+/**
+ * 文件大小格式化
+ *
+ * @param  $ :$bytes, int 文件大小
+ * @param  $ :$precision int  保留小数点
+ * @return :string
+ */
+function size_format($bytes, $precision = 2){
+	if ($bytes == 0) return "0 B";
+	$unit = array(
+			'TB' => 1099511627776,  // pow( 1024, 4)
+			'GB' => 1073741824,		// pow( 1024, 3)
+			'MB' => 1048576,		// pow( 1024, 2)
+			'kB' => 1024,			// pow( 1024, 1)
+			'B ' => 1,				// pow( 1024, 0)
+	);
+	foreach ($unit as $un => $mag) {
+		if (doubleval($bytes) >= $mag)
+			return round($bytes / $mag, $precision).' '.$un;
+	}
+}
+
+$size = get_filesize("C:/1.rmvb");
+$format = size_format($size,2);
+echo $format;
